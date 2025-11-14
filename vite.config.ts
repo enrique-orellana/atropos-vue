de1d5c716087d2842302f87ffb1c92ee4ac4f510
Julio Orellana <coppro.cman@proton.me>
2025-11-14 19:08:23 +0200

    feat: add package build configuration

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'AtroposVue',
+      formats: ['es', 'cjs'],
+      fileName: (format) => (format === 'es' ? 'index.js' : 'index.cjs')
+    },
+    rollupOptions: {
+      external: ['vue', 'atropos'],
+      output: {
+        globals: {
+          vue: 'Vue',
+          atropos: 'Atropos'
+        }
+      }
+    },
+    sourcemap: true,
+    emptyOutDir: false
+  }
+})
